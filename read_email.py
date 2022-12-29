6f82de979d4dacbd420fec5ea6417cf8ae09a8ca
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2022-12-29 11:13:47 +0000

    Remove unused get_header_data()

--- read_email.py
+++ read_email.py
@@ -1,5 +1,4 @@
 import email
-from email.header import decode_header
 import re
 
 
@@ -11,24 +10,13 @@ def get_house_urls(email_body, pattern):
     return urls_list
 
 
-def get_header_data(email_msg, key):
-    value, encoding = decode_header(email_msg[key])[0]
-    if isinstance(value, bytes):
-        value = value.decode(encoding)
-    return value
-
-
 def check_mailbox(imap, website):
     msg_count = int(imap.select(mailbox=website['mailbox'])[1][0])
     for i in range(msg_count, 0, -1):
         msg = imap.fetch(str(i), "(RFC822)")[1][0]
         msg = email.message_from_bytes(msg[1])
-        subject = get_header_data(msg, "Subject")
-        sender = get_header_data(msg, "From")
         body = msg.get_payload(decode=True).decode()
         urls = get_house_urls(body, website['url_pattern'])
-    # now scrape the urls (ex: 'https://www.casa.it/immobili/45454130' )
-    # read from config.json
     imap.close()
     imap.logout()
 
